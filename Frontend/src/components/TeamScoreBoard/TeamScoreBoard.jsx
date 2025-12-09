2a5a97e417cd4e472d3c5a7f4a2b3f3f43383a67
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-12-09 11:53:48 +0100

    update TeamScoreBoard - temporary

--- Frontend/src/components/TeamScoreBoard/TeamScoreBoard.jsx
+++ Frontend/src/components/TeamScoreBoard/TeamScoreBoard.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+import { useGame } from "../context/GameContext";
 import styles from "./TeamScoreBoard.jsx";
 
-export default function TeamScoreBoard() {}
+export default function TeamScoreBoard({ teams }) {
+  const { lastValue } = useGame();
+
+  return (
+    <div className={styles.scoreboardContainer}>
+        {teams.map((team) => 
+        <div className={styles.team}>
+            <div className={styles.imageContainer}><img src={team.image} alt={team.name} /></div>
+        </div>
+            )}
+    </div>
+  )
+}
