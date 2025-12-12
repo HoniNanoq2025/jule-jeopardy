ffc7622c81de2214718fec437212bb28e7399b82
wali100z <adanwali501@gmail.com>
2025-12-12 11:38:26 +0100

    sne

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import styles from "./Header.module.css";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-return (
-<div className={styles.Header}>
-  <Link to ="/"> Jule<span className={styles.jeopardy}>Jeopardy</span>
-  </Link>
-  </div>
-
-)
-
+  return (
+    <div className={styles.Header}>
+      <div className={styles.snow}></div>
+      <Link to="/">
+        Jule<span className={styles.jeopardy}>Jeopardy</span>
+      </Link>
+    </div>
+  );
 }
